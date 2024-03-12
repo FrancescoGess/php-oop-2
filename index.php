b043ae1acca448d55a274c92139728ffdd340e65
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-03-12 16:46:54 +0100

    creo require dei models

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-    include_once __DIR__ .'/models/Product.php'
+    include_once __DIR__ .'/models/Product.php';
 
+    require_once __DIR__ . "/models/Product.php";
+    require_once __DIR__ . "/models/Category.php";
+    require_once __DIR__ . "/models/Toy.php";
+    
+
+    $dogs = new Category( 'dogs', 'fa-solid fa-dog');
+    $cats = new Category( 'cats', 'fa-solid fa-cat');
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +21,7 @@
 
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css' integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==' crossorigin='anonymous' />
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
 
